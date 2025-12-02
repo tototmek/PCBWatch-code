58b2801d2d22b09b9f28d8543718b1b43c9bfffe
Tomek Zebrowski <zebromek@gmail.com>
2025-12-02 13:49:28 +0100

    Add simple font management. TODO: display font as one action to avoid characters occluding each other

--- epd-test/simulation/test.cpp
+++ epd-test/simulation/test.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "simulator.h"
-#include "../../graphics/output/bitmaps/bitmap_logo_64x64.h"
+#include "../../graphics/output/bitmaps/bitmap_battery.h"
+#include "../../graphics/output/fonts/font_big.h"
+#include "../../graphics/output/fonts/font_small.h"
 
 void EPD_SetFrameMemory(const uint8_t* image_buffer, int x, int y, int image_width, int image_height);
+void EPD_DrawFont(int x, int y, font_t font, int char_idx, int seq_n=0);
+
 #define EPD_WIDTH kNumPixels
 #define EPD_HEIGHT kNumPixels
 
@@ -10,10 +14,13 @@ int main()
 {
     Simulator sim;
     Simulator_Init(&sim);
-    EPD_SetFrameMemory(BITMAP_LOGO_64X64.data, 14, 12, BITMAP_LOGO_64X64.width, BITMAP_LOGO_64X64.height);
-
+    EPD_SetFrameMemory(BITMAP_BATTERY.data, 150, 174, BITMAP_BATTERY.width, BITMAP_BATTERY.height);
+    EPD_DrawFont(43, 43, FONT_BIG, 2, 0);
+    EPD_DrawFont(43, 43, FONT_BIG, 1, 1);
+    EPD_DrawFont(43, 43, FONT_BIG, 3, 2);
+    EPD_DrawFont(43, 43, FONT_BIG, 7, 3);
+    EPD_DrawFont(120, 150, FONT_SMALL, 7, 3);
     while(true) {
-        
         Simulator_Update(&sim);
         if (Simulator_CheckExit(&sim)) {
             break;
@@ -50,28 +57,15 @@ void EPD_SetFrameMemory(
   int true_start = x & 0xF8;
   int startbits = x - true_start;
   int true_end = x_end;
-  if (x_end % 8 != 7) {
-    true_end = (x_end & 0xF8) + 7;
-  }
-  std::cout<<"x_start="<<int(x)<<", true_start="<<int(true_start)<<std::endl;
-  std::cout<<"x_end="<<int(x_end)<<", true_end="<<int(true_end)<<std::endl;
+
+  true_end = ((x_end + 1) & 0xF8) + 8;
+//   std::cout<<"x_start="<<int(x)<<", true_start="<<int(true_start)<<std::endl;
+//   std::cout<<"x_end="<<int(x_end)<<", true_end="<<int(true_end)<<std::endl;
 
   int endbits = true_end - x_end;
-//   image_width &= 0xF8;
-//   if (x + image_width >= EPD_WIDTH) {
-//     x_end = EPD_WIDTH - 1;
-//   } else {
-//     x_end = x + image_width - 1;
-//   }
-//   if (y + image_height >= EPD_HEIGHT) {
-//     y_end = EPD_HEIGHT - 1;
-//   } else {
-//     y_end = y + image_height - 1;
-//   }
-  EPD_SetMemoryArea(true_start, y, true_end, y_end);
+  EPD_SetMemoryArea(true_start, y, true_end - 1, y_end);
   EPD_SetMemoryPointer(true_start, y);
   EPD_SendCommand(WRITE_RAM);
-  /* send the image data */
   int bit_idx = 0;
   uint8_t newbyte;
   int databit_idx = 0;
@@ -79,8 +73,6 @@ void EPD_SetFrameMemory(
     newbyte = 0;
     bit_idx = startbits;
     for (int i = 0; i < image_width; ++i) {
-        // newbyte |= image_buffer[databyte_idx] & (1 << (7 - databit_idx++));
-        // newbyte |= 1 <<(7-bit_idx++);
         newbyte |= GetNthBit(image_buffer, databit_idx++) << (7-bit_idx++);
         if (bit_idx >= 8) {
             bit_idx = 0;
@@ -90,4 +82,16 @@ void EPD_SetFrameMemory(
     }
     EPD_SendData(newbyte);
   }
+}
+
+
+
+void EPD_DrawFont(int x, int y, font_t font, int char_idx, int seq_n) {
+    EPD_SetFrameMemory(
+            font.data + char_idx * font.charDatalen,
+            x + seq_n * (font.charWidth + font.charSpacing),
+            y,
+            font.charWidth,
+            font.charHeight
+        );
 }
